938ab4ff8a17bc91eaf22ce148e39c6423ae1214
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-28 03:52:41 -0700

    feat: react-router installed

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+import {FaGithub } from 'react-icons/fa';
+import {Link} from "react-router-dom";
+import PropTypes from 'prop-types';
+
+function Navbar({title}) {
+  return (
+    <div>Navbar</div>
+  )
+}
+Navbar.defaultProps = {
+ title: 'Github Finder' 
+}
+
+Navbar
+export default Navbar
