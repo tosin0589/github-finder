2e2d4d93ad763452c65dd33a35e73ead2c530e5e
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-28 05:30:33 -0700

    feat: created pages for the app

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div>
+      <h1 className= "text-gxl mb-4"> Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        An app that helps with searching for profiles in Github
+      </p>
+
+
+    </div>
+  )
+}
+
+export default About
