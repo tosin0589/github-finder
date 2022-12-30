e2a34e3d77757c6cc13fe3d0a742713c04940bd8
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-29 22:40:54 -0700

    style: updated class

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 function NotFound() {
   return (
     <div className ="hero">
+      <div className= "text-center hero content"></div>
       <div className= "max-w-lg">
         <h1 className ="text-8xl font-bold mb-8">
           OOPS!
